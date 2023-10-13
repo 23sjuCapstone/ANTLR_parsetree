6ddd6a797a086042b7e02986f85693c4c6c86a26
김조현 <kimjohyun298@gmail.com>
2023-10-14 01:26:19 +0900

    gen 파일 전부 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import java.io.IOException;
 
-import org.gen.grammer.MySqlLexer;
+import org.gen.MySqlLexer;
 
 // Lexer 파일 생성 -> Tokenize -> Parser 파일 생성
 // 생성된 파스트리를 객체로 사용하기 위해서는 antlr4-tools를 설치해야 하는데
@@ -29,13 +29,13 @@ public class Main {
         // creating ANTLR related objects
         // input : Charstream  >> static helper methods where we can create a char stream from a file
 
-//        CharStream charStream = CharStreams.fromString("SELECT branch_name, customer_name FROM depositor, account WHERE depositor.account_number = account.account_number UNION SELECT branch_name, customer_name FROM borrower, loan WHERE borrower.loan_number = loan.loan_number;");
-//        MySqlLexer mySqlLexer = new MySqlLexer(charStream);
-//        CommonTokenStream commonTokenStream = new CommonTokenStream(mySqlLexer);
-//        MySqlParser mySqlParser = new MySqlParser(commonTokenStream);
+        CharStream charStream = CharStreams.fromString("SELECT branch_name, customer_name FROM depositor, account WHERE depositor.account_number = account.account_number UNION SELECT branch_name, customer_name FROM borrower, loan WHERE borrower.loan_number = loan.loan_number;");
+        MySqlLexer mySqlLexer = new MySqlLexer(charStream);
+        CommonTokenStream commonTokenStream = new CommonTokenStream(mySqlLexer);
+        MySqlParser mySqlParser = new MySqlParser(commonTokenStream);
 
         // 이제 파서에서 translationunit을 부를 수 있다 ! > parsetree를 반환값으로 준다
-//        ParseTree parseTree = mySqlParser.translationunit();
+        ParseTree parseTree = mySqlParser.translationunit();
         System.out.println("done");
 
         // #2. (Stack overflow) Is there a simple example of using the ParseTreeWalker?
